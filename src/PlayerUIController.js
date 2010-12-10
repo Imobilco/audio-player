98dda4edf0df42f61c51d2ba3cd6682ed6b0d63f
Sergey Chikuyonok <serge.che@gmail.com>
2010-12-10 20:32:48 +0300

    Removed cruft

--- src/PlayerUIController.js
+++
@@ -1,8 +0,0 @@
-/**
- * Playlist UI controller class. Controls playback and user interacton 
- * on distinct playlist tracks 
- * @author Sergey Chikuyonok (serge.che@gmail.com)
- * @link http://chikuyonok.ru
- */function PlayerUIController(playlist, ctx) {
-	
-}
